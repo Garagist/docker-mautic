68229fa432469fc9465c9416bc14a4e451c77679
David Spiola <ds@ds-mac.local>
2022-09-27 08:16:33 +0200

    Update: mautic and custome script execution handling

--- root/mode.sh
+++ root/mode.sh
@@ -2,22 +2,34 @@
 
 MAUTIC_CONTEXT="${MAUTIC_CONTEXT:-Development}"
 
+#handle fixuid for development environment
 if [ $MAUTIC_CONTEXT == 'Development' ]; then
-  fixuid "$@"
+  fixuid
 else
-  echo "fixuid: skipped because Production MAUTIC_CONTEXT"
-
-  DIR=/docker-entrypoint.d
-
-  if [ -d "$DIR" ]; then
-    for SCRIPT in $DIR/*; do
-      if [ -f $SCRIPT -a -x $SCRIPT ]; then
-        echo
-        echo "Run $SCRIPT ..."
-        $SCRIPT
-      fi
-    done
-  fi
-
-  exec "$@"
+  echo "fixuid: skipped because MAUTIC_CONTEXT != Development"
 fi
+
+#handle default docker-entrypoint.d scripts
+DIR=/usr/local/docker-entrypoint.d
+if [ -d "$DIR" ]; then
+  for SCRIPT in $DIR/*; do
+    if [ -f $SCRIPT -a -x $SCRIPT ]; then
+      echo "Run mautic script: $SCRIPT ..."
+      $SCRIPT
+    fi
+  done
+fi
+
+#handle docker-entrypoint.d scripts
+DIR=/data/docker-entrypoint.d
+if [ -d "$DIR" ]; then
+  for SCRIPT in $DIR/*; do
+    if [ -f $SCRIPT -a -x $SCRIPT ]; then
+      echo "Run custom script: $SCRIPT ..."
+      $SCRIPT
+    fi
+  done
+fi
+
+exec "$@"
+
