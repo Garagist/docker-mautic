24dc427a69d2c098be4c6702febb077ea23f761b
David Spiola <ds@ds-mac.local>
2022-09-27 08:15:55 +0200

    Update: upgrade fixuid

--- root/usr/local/sbin/install-fixuid.sh
+++ root/usr/local/sbin/install-fixuid.sh
@@ -2,7 +2,7 @@
 
 echo
 echo "Install Fixuid..."
-curl -SsL https://github.com/boxboat/fixuid/releases/download/v0.3/fixuid-0.3-linux-amd64.tar.gz \
+curl -SsL https://github.com/boxboat/fixuid/releases/download/v0.5.1/fixuid-0.5.1-linux-amd64.tar.gz \
   | tar -C /usr/local/bin -xzf - \
   && chown root:root /usr/local/bin/fixuid \
   && chmod 4755 /usr/local/bin/fixuid \
