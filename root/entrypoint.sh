bd73bc4942f1fb36e5c14eb392a1962b5a481188
David Spiola <ds@ds-mac.local>
2022-09-27 08:16:49 +0200

    Fix: entrypoint.sh

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -3,6 +3,6 @@
 php-fpm -D -R -y /usr/local/etc/php-fpm.conf
 
 # A place to pipe log
-( touch "/tmp/mautic.php-fpm.log"; tail -qF -n 0 /tmp/mautic.php-fpm.log | sed -u 's/WARNING: \[pool www\] child [0-9]* said into stdout: \"\(.*\)\"$/\1/' 1>&2 ) &
+( touch "/tmp/mautic.php-fpm.log"; tail -qF -n 0 /tmp/mautic.php-fpm.log | sed 's/WARNING: \[pool www\] child [0-9]* said into stdout: \"\(.*\)\"$/\1/' 1>&2 ) &
 
-caddy run -config /etc/Caddyfile
+caddy run --config /etc/Caddyfile
