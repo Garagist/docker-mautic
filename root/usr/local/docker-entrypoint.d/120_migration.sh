a78e5f2bd748b9659a080734c3a629a4adf417a4
David Spiola <ds@ds-mac.local>
2022-09-30 14:52:24 +0200

    Update: cleanup startup script

--- root/usr/local/docker-entrypoint.d/120_migration.sh
+++ root/usr/local/docker-entrypoint.d/120_migration.sh
@@ -3,11 +3,11 @@
 MAUTIC_RUN_MIGRAION="${MAUTIC_RUN_MIGRAION:-'true'}"
 
 if [ $MAUTIC_RUN_MIGRAION == 'true' ]; then
-    echo -e "Startup: run doctine migration\n"
+    echo -e "run doctine migration\n"
     cd /data
     php bin/console doctrine:migrations:migrate --no-interaction
 else
-    echo -e "Startup: doctine migration skipped >>\n"
+    echo -e "doctine migration skipped >>\n"
 fi;
 
 exit 0
