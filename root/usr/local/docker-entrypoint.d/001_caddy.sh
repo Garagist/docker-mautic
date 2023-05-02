71149dc211722f71b134da71795d96db01e0cf33
David Spiola <david@spiola.com>
2023-05-02 16:38:33 +0200

    Update: improve MAUTIC_SSL_ENABLED handling

--- root/usr/local/docker-entrypoint.d/001_caddy.sh
+++ root/usr/local/docker-entrypoint.d/001_caddy.sh
@@ -2,8 +2,13 @@
 
 echo "setup caddy"
 
-schema="http"
-if [[ "${MAUTIC_SSL_ENABLED}" == "true" ]] && [[  -n "$MAUTIC_TLS_EMAIL" ]]; then
+if test "$(echo "$MAUTIC_SSL_ENABLED" | tr '[:upper:]' '[:lower:]')" = "true"; then
+  ssl_enabled=true
+else
+  ssl_enabled=false
+fi
+
+if [[ "${ssl_enabled}" == "true" ]] && [[  -n "$MAUTIC_TLS_EMAIL" ]]; then
   sed -e "s/\$TLS/tls ${MAUTIC_TLS_EMAIL}/g" -e "s/\$HTTP_SCHEMA/https/g" /usr/local/etc/caddy/Caddyfile > /home/docker/Caddyfile
 else
   sed -e "s/\$TLS/ /g" -e "s/\$HTTP_SCHEMA/http/g" /usr/local/etc/caddy/Caddyfile > /home/docker/Caddyfile
