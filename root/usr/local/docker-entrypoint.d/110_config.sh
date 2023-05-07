6da3346de961758b5dacead614cd075534882b50
David Spiola <david@spiola.com>
2023-05-07 20:44:40 +0200

    FIx: logs output directory

--- root/usr/local/docker-entrypoint.d/110_config.sh
+++ root/usr/local/docker-entrypoint.d/110_config.sh
@@ -5,6 +5,7 @@ echo "setup directories"
 mkdir -p /data/docroot/app/config/
 mkdir -p /config/
 mkdir -p /cache/
+mkdir -p /logs/
 
 cp /usr/local/etc/mautic/paths_local.php /data/docroot/app/config/
 
