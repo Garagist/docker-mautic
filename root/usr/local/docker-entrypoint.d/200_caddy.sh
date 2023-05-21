f4500da6bfca6f6d8e04ca2d95444b1eff7817c7
David Spiola <david@spiola.com>
2023-05-21 13:55:59 +0200

    Update: refactor caddy startup script

--- root/usr/local/docker-entrypoint.d/200_caddy.sh
+++ root/usr/local/docker-entrypoint.d/200_caddy.sh
@@ -3,13 +3,29 @@ source /usr/local/sbin/checks.sh
 echo "setup caddy"
 
 ssl_enabled=$(check_true "$MAUTIC_SSL_ENABLED")
-
 if [[ "${ssl_enabled}" == "true" ]] && [[  -n "$MAUTIC_TLS_EMAIL" ]]; then
-  sed -e "s#TLS#tls ${MAUTIC_TLS_EMAIL}#g" -e "s#SCHEMA#${MAUTIC_DOMAIN}#g" /usr/local/etc/caddy/Caddyfile > /mautic/Caddyfile
+  SCHEMA="https://"
+  TLS="tls ${MAUTIC_TLS_EMAIL}"
   echo "tls enabled"
 else
-  sed -e "s#TLS##g" -e "s#SCHEMA#http://${MAUTIC_DOMAIN}#g" /usr/local/etc/caddy/Caddyfile > /mautic/Caddyfile
+  TLS=""
+  SCHEMA="http://"
   echo "tls disabled"
 fi
+
+DOMAINS=""
+for item in $MAUTIC_DOMAIN
+do
+    if [ -z "$DOMAINS" ]
+    then
+      DOMAINS="${SCHEMA}${item}"
+    else
+      DOMAINS="$DOMAINS, ${SCHEMA}${item}"
+    fi
+done
+
+echo "Domains ${DOMAINS}"
+
+sed -e "s#TLS#${TLS}#g" -e "s#DOMAINS#${DOMAINS}#g" /usr/local/etc/caddy/Caddyfile > /mautic/Caddyfile
 
 exit 0
