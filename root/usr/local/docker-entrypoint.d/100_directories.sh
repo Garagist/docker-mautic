878378d8c07f4163dcb83ebf628b1534e07941c8
David Spiola <david@spiola.com>
2023-11-12 19:34:44 +0100

    Update: add missing directory mappings

--- root/usr/local/docker-entrypoint.d/100_directories.sh
+++ root/usr/local/docker-entrypoint.d/100_directories.sh
@@ -6,6 +6,8 @@ mkdir -p /mautic/config/
 mkdir -p /mautic/cache/
 mkdir -p /mautic/logs/
 mkdir -p /mautic/cron/
-
+mkdir -p /mautic/data/themes/
+mkdir -p /mautic/data/assets/
+mkdir -p /mautic/data/media/
 
 exit 0
--- root/usr/local/etc/mautic/paths_local.php
+++ root/usr/local/etc/mautic/paths_local.php
@@ -2,11 +2,12 @@
 
 $paths = [
     //customizable
-    'themes'       => 'themes',
-    'assets'       => 'media',
+    'themes'       => '/mautic/data/themes',
+    'assets'       => '/mautic/data/assets',
     'asset_prefix' => '',
-    'plugins'      => 'plugins',
-    'translations' => 'translations',
+    'image_path'   => '../../mautic/data/media',
+    'plugins'      => '/mautic/data/plugins',
+    'translations' => '/mautic/data/translations',
     'local_config' => '/mautic/config/local.php',
     'cache_path' => '/mautic/cache',
     'tmp_path' => '/tmp/mautic',
